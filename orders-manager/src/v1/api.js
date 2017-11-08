fe2045e4ba71d2ee97685be9e1f2293e280a498f
Patryk Konior <patryk@arteia.co>
2017-11-08 18:10:18 +0100

    basic crud for orders created

--- orders-manager/src/v1/api.js
+++ orders-manager/src/v1/api.js
@@ -3,7 +3,8 @@ const express = require('express');
 module.exports = (mongodb) => {
     const app = express();
 
-    app.use('/orders', require('./routes/orders')(mongodb.collections('orders')));
+    const ordersRepo = require('./repositories/orders')(mongodb.collection('orders'));
+    app.use('/orders', require('./routes/orders')(ordersRepo));
 
     return app;
 };
