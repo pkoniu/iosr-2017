892f2092904e99a8b799beb6be1f78c056f46fa9
Patryk Konior <patryk@arteia.co>
2017-11-22 15:48:53 +0100

    little change to menu manager routes

--- menu-manager/src/v1/api.js
+++ menu-manager/src/v1/api.js
@@ -4,7 +4,7 @@ module.exports = (mongodb) => {
     const app = express();
 
     const ordersRepo = require('./repositories/menu')(mongodb.collection('orders'));
-    app.use('/menu', require('./routes/menu')(ordersRepo));
+    app.use('/menu/items', require('./routes/menu')(ordersRepo));
 
     return app;
 };
